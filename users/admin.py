928d520e74eb16a28018e3a9292986f3b255805b
Alexander Schischkin <schischkin2@yandex.com>
2026-08-21 04:53:26 +0200

    feat(users): payment model with migration and admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'city', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('email', 'city')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'payment_date', 'paid_course', 'paid_lesson', 'amount', 'payment_method')
+    list_filter = ('payment_method', 'payment_date', 'paid_course', 'paid_lesson')
+    search_fields = ('user__email',)
